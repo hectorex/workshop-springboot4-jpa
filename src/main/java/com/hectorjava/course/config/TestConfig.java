5820eef139cc30e8bb9f0315128f1eed18c43d21
hectorex <celsohectorhm@gmail.com>
2026-03-19 18:21:32 -0400

    Product-OrderItem one-to-many association

--- src/main/java/com/hectorjava/course/config/TestConfig.java
+++ src/main/java/com/hectorjava/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.hectorjava.course.entities.Category;
 import com.hectorjava.course.entities.Order;
 import com.hectorjava.course.entities.OrderItem;
+import com.hectorjava.course.entities.Payment;
 import com.hectorjava.course.entities.Product;
 import com.hectorjava.course.entities.User;
 import com.hectorjava.course.entities.enums.OrderStatus;
@@ -80,6 +81,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRespository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 		
 	}
 }
